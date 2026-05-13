8e720c0176b54a4ffa2bdf482656fb3fc5a6f507
Nghia Tran <nghiatrandtvt@gmail.com>
2026-05-13 15:34:39 +0200

    Improve search

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,3 +1,16 @@
+-- Quickfix: open entries in tab/split instead of current buffer
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'qf',
+  callback = function()
+    local map = vim.keymap.set
+    local opts = { buffer = true, noremap = true }
+    map('n', '<CR>',  '<CR>',              opts)  -- default: open in current window
+    map('n', 't',     '<C-W><CR><C-W>T',   opts)  -- open in new tab
+    map('n', 'x',     '<C-W><CR><C-W>K',   opts)  -- open in horizontal split
+    map('n', 'v',     '<C-W><CR><C-W>H',   opts)  -- open in vertical split
+  end,
+})
+
 -- Terminal: disable line numbers and set background
 vim.api.nvim_create_autocmd({ 'BufEnter', 'TermOpen', 'BufRead' }, {
   pattern = 'term://*',
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,12 +1,29 @@
 local map = vim.keymap.set
 
--- Search in current file
+-- Search in current file or directory: pattern then optional path with tab completion
 map('n', 's', function()
   local pattern = vim.fn.input('Search: ')
-  if pattern ~= '' then
+  if pattern == '' then return end
+  local dir = vim.fn.input('Directory (empty = current file): ', '', 'dir')
+  if dir == '' then
     vim.cmd('vimgrep /' .. pattern .. '/ % | copen')
+  else
+    vim.cmd('grep! -r ' .. pattern .. ' ' .. dir)
+    vim.cmd('copen')
   end
-end, { desc = 'search-in-file' })
+end, { desc = 'search-file-or-dir' })
+
+-- Search in all open buffers
+map('n', 'S', function()
+  local pattern = vim.fn.input('Search: ')
+  if pattern == '' then return end
+  local files = table.concat(vim.tbl_map(function(buf)
+    return vim.api.nvim_buf_get_name(buf)
+  end, vim.tbl_filter(function(buf)
+    return vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_name(buf) ~= ''
+  end, vim.api.nvim_list_bufs())), ' ')
+  vim.cmd('vimgrep /' .. pattern .. '/ ' .. files .. ' | copen')
+end, { desc = 'search-open-buffers' })
 
 -- Exit terminal mode
 map('t', '<Esc>', '<C-\\><C-n>', { desc = 'exit-terminal-mode' })
